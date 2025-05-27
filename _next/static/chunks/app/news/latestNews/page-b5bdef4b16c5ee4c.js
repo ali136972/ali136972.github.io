e7821e6718fbcb329663bdb7717ff84882070541
ali136972 <133485050+ali136972@users.noreply.github.com>
2025-05-27 09:24:33 +0330

    Add files via upload

---
+++ _next/static/chunks/app/news/latestNews/page-b5bdef4b16c5ee4c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[99,409,555,834,103,240,542],{3521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
