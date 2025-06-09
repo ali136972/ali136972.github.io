393e0be202b0576bb72839772700b09e66ca0883
AliHeydari <www.morvarid.ali69.ah@gmail.com>
2025-06-09 14:07:40 +0330

    app

--- app/employers/EmployersList.jsx
+++ app/employers/EmployersList.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import React from 'react';
 
 async function getEmployers(){
   //adding More Delay
--- app/users/UsersList.jsx
+++ app/users/UsersList.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import React, { Suspense } from 'react';
 
 async function getUsers(){
   //adding More Delay
