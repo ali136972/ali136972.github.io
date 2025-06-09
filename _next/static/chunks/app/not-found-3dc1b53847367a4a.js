27168cbf3d4fecd7cd761ab2c323d3c53b5bda5b
AliHeydari <www.morvarid.ali69.ah@gmail.com>
2025-06-09 13:26:33 +0330

    app

--- _next/static/chunks/app/not-found-3dc1b53847367a4a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,819,194,478,978],{4152:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=4152)}),_N_E=n.O()}]);
